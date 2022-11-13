15005975a71e7ccf02541879ab105052a6f6dbd2
espmaster <serghei.fironov@stud.uni-due.de>
2022-11-13 19:29:50 +0100

    + Liga-Daten(Liga, Spieltage, Spiele) für Nutzer

--- src/main/java/sep/tippspiel/systemadministrator/SystemadministratorRepository.java
+++ src/main/java/sep/tippspiel/systemadministrator/SystemadministratorRepository.java
@@ -13,4 +13,14 @@ public interface SystemadministratorRepository extends JpaRepository<Systemadmin
 
     @Query("SELECT sa FROM Systemadministrator sa where sa.vorname= : vorname")
     List<Systemadministrator> findByName(@Param("vorname") String vorname);
+
+/*    SELECT  l.name, t.tag, m1.name, s.score, m2.name
+    FROM SPIEL s
+    JOIN MANNSCHAFT m1 ON s.MANNSCHAFT_ID=m1.ID
+    JOIN MANNSCHAFT m2 ON s.MANNSCHAFT_ID2=m2.ID
+    JOIN SPIELTAG t ON s.SPIELTAG_ID=t.ID
+    JOIN SPIELPLAN p ON p.ID=t.SPIELPLAN_ID
+    JOIN LIGA l ON l.SPIELPLAN_ID=p.ID
+
+    ORDER BY t.tag*/
 }
