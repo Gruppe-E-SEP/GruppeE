ad8b9b6973fb49101daf7e832528e80fa6fe633f
Kevin <kevin.prinz@uni-due.de>
2022-12-13 16:45:16 +0100

    Mail Fukntionierend

--- src/main/java/sep/tippspiel/mailingsystem/SendMail.java
+++ src/main/java/sep/tippspiel/mailingsystem/SendMail.java
@@ -1,53 +1,105 @@
 package sep.tippspiel.mailingsystem;
-import java.util.Properties;
 
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
+import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
+import org.apache.commons.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import sep.tippspiel.user.Users;
 
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.file.Paths;
+import java.security.GeneralSecurityException;
+import java.util.Properties;
+import java.util.Set;
+
 public class SendMail {
+    private final Gmail service;
     @Autowired Users users;
-
-    public static void main(String[] args){
-        String to = "kevinprinz.1@web.de";
-
-        String from = "tippspiel@gmail.com";
-
-        String host = "smtp.gmail.com";
-
-        Properties properties = System.getProperties();
-
-        properties.put("mail.smtp.host", host);
-        properties.put("mail.smtp.port", "465");
-        properties.put("mail.smtp.ssl.enable", "true");
-        properties.put("mail.smtp.auth", "true");
-
-        Session session = Session.getInstance(properties,new javax.mail.Authenticator() {
-            protected PasswordAuthentication getPasswordAuthentication(){
-                return new PasswordAuthentication("tippspiel", "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");
-            }
-        });
-        session.setDebug(true);
-        try {
-            MimeMessage message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(from));
-            message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
-            message.setSubject("Your Authentication Code!");
-            message.setText("Code");
-            System.out.println("sending ...");
-            Transport.send(message);
-            System.out.println("Sent message sucessfully!");
-        }
-        catch (MessagingException mex){
-            mex.printStackTrace();
-        }
-
-
-
-
+    private static final String TEST_EMAIL = "kevinprinz.1@web.de";
+    public SendMail() throws Exception{
+        NetHttpTransport httpTransport = GoogleNetHttpTransport.newTrustedTransport();
+        GsonFactory jsonFactory = GsonFactory.getDefaultInstance();
+        service = new Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, jsonFactory))
+                .setApplicationName("Test Mail")
+                .build();
 
     }
+
+    private static Credential getCredentials(final NetHttpTransport httpTransport, GsonFactory jsonFactory)
+            throws IOException {
+
+        GoogleClientSecrets clientSecrets =
+                GoogleClientSecrets.load(jsonFactory, new InputStreamReader(SendMail.class.getResourceAsStream("/credentials.json")));
+
+
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                httpTransport, jsonFactory, clientSecrets, Set.of(GmailScopes.GMAIL_SEND))
+                .setDataStoreFactory(new FileDataStoreFactory(Paths.get("tokens").toFile()))
+                .setAccessType("offline")
+                .build();
+        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
+
+        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("users");
+    }
+
+    public void sendMail(String subject, String msg) throws Exception{
+
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+        MimeMessage email = new MimeMessage(session);
+        email.setFrom(new InternetAddress(TEST_EMAIL));
+        email.addRecipient(javax.mail.Message.RecipientType.TO,
+                new InternetAddress(TEST_EMAIL));
+        email.setSubject(subject);
+        email.setText(msg);
+
+        // Encode and wrap the MIME message into a gmail message
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        email.writeTo(buffer);
+        byte[] rawMessageBytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(rawMessageBytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+
+        try {
+            // Create send message
+            message = service.users().messages().send("me", message).execute();
+            System.out.println("Message id: " + message.getId());
+            System.out.println(message.toPrettyString());
+        } catch (GoogleJsonResponseException e) {
+
+            GoogleJsonError error = e.getDetails();
+            if (error.getCode() == 403) {
+                System.err.println("Unable to send message: " + e.getDetails());
+            } else {
+                throw e;
+            }
+        }
+    }
+    public static void main(String[] args) throws Exception {
+        new SendMail().sendMail("Code!", "1234");
+    }
 }
+
+
+
